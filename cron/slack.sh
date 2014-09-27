ae1bc8f1ca3e0577787573ff5233f374f6c9e5f9
root <root@ip-172-31-21-52.ap-northeast-1.compute.internal>
2014-09-27 23:52:37 +0000

    fix slack.sh

--- cron/slack.sh
+++ cron/slack.sh
@@ -6,7 +6,7 @@ Score=`/usr/bin/tail -n 1 /tmp/isucon.score  | /bin/cut -d' ' -f9`
 HEAD=`cd /home/isucon/isucon3 && /usr/bin/git rev-parse HEAD`
 
 TEXT="HEAD-sha:%60$HEAD%60"
-/usr/bin/curl -X POST https://slack.com/api/chat.postMessage -d "channel=#ulix&text=$TEXT&username=ISUCON3-SCORE&token=xoxp-2500638381-2580872673-2623220604-daf2fd"
+/usr/bin/curl -X POST https://slack.com/api/chat.postMessage -d "channel=#ulix_notify&text=$TEXT&username=ISUCON3-SCORE&token=xoxp-2500638381-2580872673-2623220604-daf2fd"
 
-TEXT="Result:$RESULT%20%20RawScore:$RawScore%20%20Fails:$Fails%20%20Score:$Score"
-/usr/bin/curl -X POST https://slack.com/api/chat.postMessage -d "channel=#ulix&text=$TEXT&username=ISUCON3-SCORE&token=xoxp-2500638381-2580872673-2623220604-daf2fd"
+TEXT="Result:$RESULT%20%20RawScore:$RawScore%20%20Fails:$Fails%20%20Score:$Score%20@channel"
+/usr/bin/curl -X POST https://slack.com/api/chat.postMessage -d "channel=#ulix_notify&text=$TEXT&username=ISUCON3-SCORE&token=xoxp-2500638381-2580872673-2623220604-daf2fd"
