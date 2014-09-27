e4239a9982f99a8a245bc72fc1ed10f2b443aee6
oppai <hotsoup.h@gmail.com>
2014-09-27 20:34:14 +0000

    fix pull.sh && add pull.sh

--- cron/pull.sh
+++ cron/pull.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
-git stash
-git pull origin `git rev-parse --abbrev-ref HEAD`
-git stash pop
+cd /home/isucon/isucon3
+/usr/bin/git stash
+/usr/bin/git pull origin `/usr/bin/git rev-parse --abbrev-ref HEAD`
+/usr/bin/git stash pop
