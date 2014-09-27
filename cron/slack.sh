d3012fec2092552d25ebd4b862922f63e8b86038
oppai <hotsoup.h@gmail.com>
2014-09-27 18:08:49 +0000

    fix crontab to chenge directory into isucon3

--- cron/slack.sh
+++ cron/slack.sh
@@ -3,7 +3,7 @@ RESULT=`tail -n 1 /tmp/isucon.score  | cut -d' ' -f2`
 RawScore=`tail -n 1 /tmp/isucon.score  | cut -d' ' -f5`
 Fails=`tail -n 1 /tmp/isucon.score  | cut -d' ' -f7`
 Score=`tail -n 1 /tmp/isucon.score  | cut -d' ' -f9`
-HEAD=`git rev-parse HEAD`
+HEAD=`cd /home/isucon/isucon3 && git rev-parse HEAD`
 
 TEXT="HEAD-sha:%60$HEAD%60"
 curl -X POST https://slack.com/api/chat.postMessage -d "channel=#ulix&text=$TEXT&username=ISUCON3-SCORE&token=xoxp-2500638381-2580872673-2623220604-daf2fd"
